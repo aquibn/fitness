a846e89edd02e63e7dc16e90481971b9f676c6c3
aquibn <naweeddoodle3@gmail.com>
2021-06-27 23:34:52 +0530

    deploying heroku

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -33,17 +33,22 @@ export class AboutComponent implements OnInit {
 
     this.fitnessService.bmi(form.value.age,form.value.weight,form.value.height).subscribe(data =>{
       this.BMI = data;
+      console.log(this.BMI);
     });
     this.fitnessService.macroamount(form.value.gender,form.value.height,form.value.weight,form.value.activity,form.value.goal).subscribe(data=>{
       this.macroAmount=data;
+      console.log(this.macroAmount);
 
     });
     this.fitnessService.mets().subscribe(data=>{
      this.mets=data;
+     console.log(this.mets);
 
     });
     this.fitnessService.fatpercent(form.value.age,form.value.gender,form.value.height,form.value.weight,form.value.neck,form.value.waist,form.value.hip).subscribe(data=>{
       this.Fatpercent=data;
+      console.log(this.Fatpercent);});
+
       this.fitnessService.idealweight(form.value.gender,form.value.height,form.value.weight).subscribe(data=>{
       this.idealweight=data;
       console.log(this.idealweight);
@@ -54,7 +59,7 @@ export class AboutComponent implements OnInit {
       console.log(this.macronutrient);
     })
 
-    })
+
 
 
 
